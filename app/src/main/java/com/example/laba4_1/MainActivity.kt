e492075e32f1182c2dadcf5b5aa19c9482e4a514
matatapol22 <matatapol22@gmail.com>
2024-10-18 00:32:16 +0700

    Задала список вопросов и переменные

--- app/src/main/java/com/example/laba4_1/MainActivity.kt
+++ app/src/main/java/com/example/laba4_1/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.laba4_1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val questionBank = listOf(
+        Question("Ты человек?", true),
+        Question("Осьминог водоплавающее?", true),
+        Question("2 + 2 = 5?", false)
+    )
+
+    private var currentIndex = 0
+    private var correctAnswers = 0
+
+    private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
+
+
 }
+data class Question(val text: String, val answer: Boolean)
