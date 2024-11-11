1db644018607d52f924a03f4184e75f6922afb64
matatapol22 <matatapol22@gmail.com>
2024-11-11 15:42:40 +0700

    Создала класс который будет сохранять состояние для MainActivity

--- app/src/main/java/com/example/laba4_1/QuizViewModel.kt
+++ app/src/main/java/com/example/laba4_1/QuizViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.laba4_1
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 
-class QuizViewModel {
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
 }
